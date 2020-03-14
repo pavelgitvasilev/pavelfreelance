ec3b68ad6ed3903fc99b71e202313e851aaa15df
PavelVasilev <vpnprint@gmail.com>
2020-03-14 13:46:54 +0300

    feat:add keyUp on ESC and hidden without modal block

--- js/main.js
+++ js/main.js
@@ -2,15 +2,32 @@ document.addEventListener('DOMContentLoaded', function(event){
 
  const modal = document.querySelector('.modal');
  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
- const CloseBtn = document.querySelector('.modal__close')
+ const CloseBtn = document.querySelector('.modal__close');
+ const dark = document.querySelector('.wrap')
  const switchModal = () => {
   modal.classList.toggle('modal--visible');
+  document.addEventListener('keyup', function(e){
+   let key = e.keyCode;
+   if(key == 27) {
+    modal.classList.toggle('modal--visible');
+    switchModal();
+   }
+  })
+  dark.classList.toggle('wrap--dark');
+
  }
 
  modalBtn.forEach(element => {
-  element.addEventListener('click', switchModal)
+  element.addEventListener('click', switchModal);
  });
  
- CloseBtn.addEventListener('click', switchModal)
- 
+ CloseBtn.addEventListener('click', switchModal);
+ dark.addEventListener('click',switchModal);
 });
+
+// window.addEventListener('click', function (e) {
+//  if(e.target == modal) {
+//   modal.style.display = 'none';
+//  }
+// })
+ 
