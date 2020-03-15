92db7048c67bcc6c30a176f4623afb445d88628b
PavelVasilev <vpnprint@gmail.com>
2020-03-15 22:12:42 +0300

    the next test

--- js/main.js
+++ js/main.js
@@ -72,6 +72,25 @@ $(document).ready(function () {
    }
 }); //нажатие вне окна
 
+  //initialize swiper when document ready
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },    
+  }) // слайдер
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  next.css('left', prev.width() + 10 + bullets.width() + 10);
+  bullets.css('left', prev.width() + 10);
+
 
 }); //закрывается документ реди
 
