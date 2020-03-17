224907579454d6360f1dfa6eca662eec30ad06f7
PavelVasilev <vpnprint@gmail.com>
2020-03-18 00:43:51 +0300

    feat: add block control and wow animate for somebody blocks

--- js/main.js
+++ js/main.js
@@ -91,6 +91,24 @@ $(document).ready(function () {
   next.css('left', prev.width() + 10 + bullets.width() + 10);
   bullets.css('left', prev.width() + 10);
 
+  new WOW().init();
+
+  var wow = new WOW(
+    {
+      boxClass:     '.video__play',      // animated element css class (default is wow)
+      animateClass: 'fadeInRight', // animation css class (default is animated)
+      offset:       0,          // distance to the element when triggering the animation (default is 0)
+      mobile:       true,       // trigger animations on mobile devices (default is true)
+      live:         true,       // act on asynchronously loaded content (default is true)
+      callback:     function(box) {
+        // the callback is fired every time an animation is started
+        // the argument that is passed in is the DOM node being animated
+      },
+      scrollContainer: null,    // optional scroll container selector, otherwise use window,
+      resetAnimation: true,     // reset animation on end (default is true)
+    }
+  );
+  wow.init();
 
 }); //закрывается документ реди
 
