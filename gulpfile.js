547c5cb930e3cbccb15d0e89643fa4188d277d01
PavelVasilev <vpnprint@gmail.com>
2020-03-26 01:28:01 +0300

    lesson 28

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
-const {src, dest, watch} = require('gulp');
+const {src, dest, watch, series} = require('gulp');
 const browserSync = require('browser-sync').create();
 const minifyCSS = require('gulp-minify-css');
 const concat = require('gulp-concat');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const minify = require('gulp-minify');
+const htmlmin = require('gulp-htmlmin');
+const tinypng = require('gulp-tinypng-compress');
 
 // Static server
 function bs() {
@@ -29,6 +33,62 @@ function serveSass() {
         .pipe(dest("./css"))
         .pipe(browserSync.stream());
 };    
+
+function buildCSS(done) {
+    src('css/**/**.css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(dest('dist/css/'));
+    done();
+}
+
+function buildJS(done) {
+    src(['js/**.js', '!js/**.min.js'])
+        .pipe(minify({
+            ext:{
+                min:'.js'
+            }
+        }))
+        .pipe(dest('dist/js/'));
+    src('js/**.min.js')
+        .pipe(dest('dist/js/'));
+        done();
+}
+
+function html(done) {
+    src('**.html')   
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(dest('dist/'));
+        done();
+}
+
+function php(done) {
+    src('**.php')   
+        .pipe(dest('dist/'));
+    src('phpmailer/**.**')
+        .pipe(dest('dist/phpmailer/'))
+        done();
+}
+
+function fonts(done) {
+    src('fonts/**/**')   
+        .pipe(dest('dist/fonts/'));
+        done();
+}
+
+function imagemin(done) {
+    src('img/**/**')
+        .pipe(tinypng({key: 'b8WCSLTzjlVxQRCgxw8zjwX2s59RyK2C'}))
+        .pipe(dest('dist/img/'))
+    src('img/**/*.svg')
+        .pipe(dest('dist/img/'))
+    src('img/**/*.jpg')
+        .pipe(tinypng({key: 'b8WCSLTzjlVxQRCgxw8zjwX2s59RyK2C'}))
+        .pipe(dest('dist/img/'))
+        done();
+}
+
 exports.serve = bs;
+exports.build = series(buildCSS, buildJS, html, php, fonts, imagemin);
+
 
 
